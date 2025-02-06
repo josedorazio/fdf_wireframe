a7c5308209545a565ff6386fee7556d4c9a38b7c
Jose Dorazio <jdorazio@c2r7s1.42madrid.com>
2025-02-06 15:30:18 +0100

    GNL function

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdorazio <jdorazio@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 11:39:24 by jdorazio          #+#    #+#             */
+/*   Updated: 2024/10/08 10:41:15 by jdorazio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <ctype.h>
+# include <aio.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char	*s, int c);
+size_t	ft_strlen(const char *s);
+
+#endif
