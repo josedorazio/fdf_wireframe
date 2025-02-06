172bbe30d523e33d83396fcff68db71e03515bb0
Jose Dorazio <jdorazio@c2r7s1.42madrid.com>
2025-02-06 16:45:48 +0100

    created fdf.h

---
+++ fdf.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdorazio <jdorazio@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 14:15:39 by jdorazio          #+#    #+#             */
+/*   Updated: 2025/02/06 15:49:53 by jdorazio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_h
+# define FDF_h
+
+# include <unistd.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <math.h>
+
+# include "libft/libft.h"
+# include "printf/ft_printf.h"
+# include "get_next_line/get_next_line.h"
+# include "minilibx/mlx.h"
+
+// Medidas de la interfaz
+# define WIDTH 1920
+# define HEIGHT 1080
+
+
+
+
+int	main(void);
+
+
+#endif
