79312374f799c37d71d883d40ad93e98939850cd
root <root@LAPTOP-KE2O475O.>
2025-02-11 23:51:47 +0100

    added keybindings.h

--- fdf.h
+++ fdf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdorazio <jdorazio@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: jdorazio <jdorazio@student.42.madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 14:15:39 by jdorazio          #+#    #+#             */
-/*   Updated: 2025/02/11 17:11:56 by jdorazio         ###   ########.fr       */
+/*   Updated: 2025/02/11 23:24:10 by jdorazio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,11 @@
 # include "printf/ft_printf.h"
 # include "get_next_line/get_next_line.h"
 # include "minilibx/mlx.h"
+# include "keybindings.h"
 
 // Medidas de la interfaz
 # define WIDTH 1920
 # define HEIGHT 1080
-# define HEIGHT_SCALE 100
-# define SCALE 10
 # define M_PI 3.14159265358979323846
 
 typedef struct s_map
@@ -37,6 +36,8 @@ typedef struct s_map
 	int	height;
 	int	*width;
 	int	**matrix;
+	int	z_max;
+	int	z_min;
 }	t_map;
 
 typedef struct s_point
@@ -58,20 +59,20 @@ typedef struct t_delta
 
 
 
-typedef struct	s_data {
+typedef struct	s_image {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
-}			t_data;
+}			t_image;
 
 
 typedef struct s_display
 {
 	void	*mlx;
 	void	*win;
-	t_data	img;
+	t_image	img;
 	t_map	map;
 }	t_display;
 
@@ -98,6 +99,7 @@ void	load_map(char *file, t_map *map);
 void	fill_matrix(int fd, t_map *map);
 void	map_init(char *file, t_map *map);
 void	map_dimensions(char *file, t_map *map);
+void	z_values(t_map *map);
 
 // #----------# MAP INIT UTILS #----------#
 void	vertical_dim(char *file, t_map *map);
@@ -107,7 +109,7 @@ char	*clear_line(int fd);
 
 // #----------# START #----------#
 void	system_init(t_map *map);
-int	init_display_mlx(t_map *map, t_display *mlx);
+int		init_display_mlx(t_map *map, t_display *mlx);
 
 
 
@@ -117,7 +119,15 @@ void	draw_map(t_display *mlx);
 void	bresenham_line(t_display *mlx, t_point p1, t_point p2);
 void	init_bresenham_line(t_point *p1, t_point *p2, t_delta *delta);
 t_point	create_point(int x, int y, t_display *mlx);
-void	isometric(t_point *p);
+void	isometric(t_point *p, t_display *mlx);
+
+
+// #----------# KEY EVENT #----------#
+int		key_hook(int keycode, t_display *mlx);
+int		free_close(t_display *mlx);
+void	free_display(t_display *mlx);
+void	free_map(t_map *map);
 
 
 #endif
+	
---
+++ keybindings.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keybindings.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdorazio <jdorazio@student.42.madrid.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 22:38:37 by jdorazio          #+#    #+#             */
+/*   Updated: 2025/02/11 23:46:38 by jdorazio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEY_BINDING_H
+# define KEY_BINDING
+
+
+/* EVENTOS*/
+# define KP 02 // Evento Presión Tecla
+# define DN 17 // Evento Cierre Ventana
+
+
+/* Teclas */
+# define ESC 53 // Tecla ESC
+
+#endif
