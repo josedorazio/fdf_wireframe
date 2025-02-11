a9af205eee4845217f92157da880b0011e6f32d3
root <root@LAPTOP-KE2O475O.>
2025-02-11 12:47:39 +0100

    last updated version

--- main.c
+++ main.c
@@ -3,30 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdorazio <jdorazio@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: jdorazio <jdorazio@student.42.madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 14:17:04 by jdorazio          #+#    #+#             */
-/*   Updated: 2025/02/06 15:48:09 by jdorazio         ###   ########.fr       */
+/*   Updated: 2025/02/10 22:15:30 by jdorazio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
 
-
-
-
-
-int	main(void)
+int	main(int ac, char **av)
 {
+	t_map	map;
 
-	void	*mlx;
+	if (ac != 2)
+		terminate(1);
+	load_map(av[1], &map);
 
-	mlx = mlx_init();
-	if (NULL == mlx)
-		return (1);
+
+	printf("Starting System Init\n");
+	system_init(&map);
+	
+	printf("Working\n");
+    // Free memory
+    for (int i = 0; i < map.height; i++)
+        free(map.matrix[i]);
+    free(map.matrix);
 
 
 	return (0);
-
 }
+
+
+
