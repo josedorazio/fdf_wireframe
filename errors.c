47591fb371349c394254087bbd5533919f46f5d8
root <root@LAPTOP-KE2O475O.>
2025-02-12 23:44:07 +0100

    error_code and frees were added. It's still not working 100% correctly

--- errors.c
+++ errors.c
@@ -6,25 +6,33 @@
 /*   By: jdorazio <jdorazio@student.42.madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:49:11 by jdorazio          #+#    #+#             */
-/*   Updated: 2025/02/10 22:14:19 by jdorazio         ###   ########.fr       */
+/*   Updated: 2025/02/12 19:02:03 by jdorazio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "fdf.h"
 
-int	terminate(int error_code)
+int	terminate(int error_code, void *ptr)
 {
-	if (error_code == 1)
-		ft_printf("NOT ENOUGH INPUTS\n");
+	if (error_code == 1) // missing inputs
+		ft_printf("NOT ENOUGH INPUTS\n"); //not an fdf file
 	else if (error_code == 2)
 		ft_printf("NOT AN .fdf file\n");
-	else if (error_code == 3) 
-		ft_printf("NOT CREATED\n");
-	else if (error_code == 4)
-		ft_printf("Map has a non-numerical character\n");
+	else if (error_code == 3)
+		// FAILS TO REED FILE
+		free(ptr);
+	else if (error_code == 4)  // Free maps
+		free_map(ptr);
 	else if (error_code == 5)
-		ft_printf("mlx failed to execute\n");
+		free(ptr);
 	else if (error_code == 6)
+		free_close(ptr);
+	else if (error_code == 7)
 		ft_printf("Failed Isometric Projection \n");
 	exit(EXIT_FAILURE);
 }
+
+/* me falta un free para fd, que en realidad 
+lo que liberaria es FILE
+
+*/
